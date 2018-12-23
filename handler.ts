d07621395d3ebd780994b2c0aeda9c832a7c8ca4
kytiken <kytiken@gmail.com>
2018-12-23 10:41:55 +0900

    add graphql api endpoint

--- handler.ts
+++ handler.ts
@@ -1,11 +1,13 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler } from "aws-lambda";
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
+export const graphql: APIGatewayProxyHandler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event,
-    }),
+      query: event.query
+    })
   };
-}
+};
