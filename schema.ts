77a0809097a2ca362dacb19b69efd4a2eacb7a57
kytiken <kytiken@gmail.com>
2018-12-23 16:41:50 +0900

    implement createPost mutation

--- schema.ts
+++ schema.ts
@@ -4,6 +4,9 @@ import {
   GraphQLString,
   GraphQLSchemaConfig
 } from "graphql";
+import { DynamoDB } from "aws-sdk";
+import { v4 as uuid } from "uuid";
+const dynamoDb = new DynamoDB.DocumentClient();
 
 const postType = new GraphQLObjectType({
   name: "Post",
@@ -14,7 +17,7 @@ const postType = new GraphQLObjectType({
   }
 });
 
-const query = new GraphQLObjectType({
+const QueryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     post: {
@@ -30,13 +33,44 @@ const query = new GraphQLObjectType({
   }
 });
 
-// const mutation: GraphQLObjectType = {
-//   name: "Mutation",
-//   fields: {}
-// };
+const MutationType = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createPost: {
+      type: postType,
+      args: {
+        title: { type: GraphQLString },
+        contents: { type: GraphQLString }
+      },
+      resolve: async (source, { title, contents }) => {
+        const newId = uuid();
+        const itemParams = {
+          id: newId,
+          title,
+          contents
+        };
+        const putParams: DynamoDB.DocumentClient.PutItemInput = {
+          TableName: process.env.DYNAMODB_TABLE,
+          Item: itemParams
+        };
+        return await dynamoDb
+          .put(putParams)
+          .promise()
+          .then(data => {
+            console.log(itemParams);
+            return itemParams;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    }
+  }
+});
 
 const config: GraphQLSchemaConfig = {
-  query
+  query: QueryType,
+  mutation: MutationType
 };
 
 export default new GraphQLSchema(config);
